5220a8a0385b40ea43dc4c7ecf5f3b5adcb7ce27
Nikodem <xmedo117@proton.me>
2026-03-11 14:23:20 +0100

    zadanie01 4.0

--- Zadanie01/zadanie01.pas
+++ Zadanie01/zadanie01.pas
@@ -5,25 +5,30 @@ type
 var
     liczby: Tablica;
 
-procedure randomizer(var tab: Tablica);
+procedure randomizer(var tab: Tablica; min, max, count: Integer);
 var
     i: Integer;
 begin
     Randomize;
-    for i := 1 to 50 do
+    if count > 50 then
     begin
-        tab[i] := Random(100)+1;
+        count := 50;
     end;
-    writeln('Wygenerowano 50 losowych liczb.')
+
+    for i := 1 to count do
+    begin
+        tab[i] := Random(max - min)+1;
+    end;
+    writeln('Wygenerowano ', count, ' losowych liczb z zakresu od ', min, ' do ', max, '.');
 end;
 
-procedure bubbleSort(var tab: Tablica);
+procedure bubbleSort(var tab: Tablica; count: Integer);
 var
     i, j, temp: Integer;
 begin
-    for i := 1 to 50-1 do
+    for i := 1 to count-1 do
     begin
-        for j := 1 to 50-i do
+        for j := 1 to count-i do
         begin
             if tab[j] > tab[j+1] then
             begin
@@ -37,6 +42,6 @@ begin
 end;
 
 begin
-    randomizer(liczby);
-    bubbleSort(liczby);
+    randomizer(liczby, 10, 50, 25);
+    bubbleSort(liczby, 25);
 end.
