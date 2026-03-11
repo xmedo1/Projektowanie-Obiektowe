2173893376a8b527682b080b22cbcbd9ba2b4fef
Nikodem <xmedo117@proton.me>
2026-03-11 15:01:59 +0100

    zadanie01 4.5

--- Zadanie01/README.md
+++ Zadanie01/README.md
@@ -3,5 +3,5 @@
 :white_check_mark: 3.0 Procedura do generowania 50 losowych liczb od 0 do 100 \
 :white_check_mark: 3.5 Procedura do sortowania liczb \
 :white_check_mark: 4.0 Dodanie parametrów do procedury losującej określającymi zakres losowania: od, do, ile \
-:x: 4.5 5 testów jednostkowych testujące procedury \
+:white_check_mark: 4.5 5 testów jednostkowych testujące procedury \
 :x: 5.0 Skrypt w bashu do uruchamiania aplikacji w Pascalu via docker
--- Zadanie01/zadanie01.pas
+++ Zadanie01/zadanie01.pas
@@ -17,7 +17,7 @@ begin
 
     for i := 1 to count do
     begin
-        tab[i] := Random(max - min)+1;
+        tab[i] := Random(max - min + 1) + min; {fixed}
     end;
     writeln('Wygenerowano ', count, ' losowych liczb z zakresu od ', min, ' do ', max, '.');
 end;
@@ -41,7 +41,61 @@ begin
     writeln('Posortowano.');
 end;
 
+procedure unitTest();
+var
+    testTab: Tablica;
+    i: Integer;
+    passed: Boolean;
+begin
+    passed := True;
+    {Test 1: Czy losowanie w zakresie}
+    randomizer(testTab, 5, 15, 5);
+    for i := 1 to 5 do
+    begin
+        if (testTab[i] < 5) or (testTab[i] > 15) then
+            passed := False;
+    end;
+
+    {Test 2: Sortowanie posortowanej tablicy}
+    testTab[1] := 1;
+    testTab[2] := 2;
+    testTab[3] := 3;
+    bubbleSort(testTab, 3);
+    if (testTab[1] <> 1) or (testTab[2] <> 2) or (testTab[3] <> 3) then 
+        passed := False;
+
+    {Test 3: Duplikaty}
+    testTab[1] := 10;
+    testTab[2] := 10;
+    testTab[3] := 10;
+    bubbleSort(testTab, 3);
+    if (testTab[1] <> 10) or (testTab[2] <> 10) or (testTab[3] <> 10) then 
+        passed := False;
+
+    {Test 4: Jeden element}
+    testTab[1] := 10;
+    bubbleSort(testTab, 1);
+    if (testTab[1] <> 10) then 
+        passed := False;
+
+    {Test 5: Sortowanie odwroconej tablicy}
+    testTab[1] := 3;
+    testTab[2] := 2;
+    testTab[3] := 1;
+    bubbleSort(testTab, 3);
+    if (testTab[1] <> 1) or (testTab[2] <> 2) or (testTab[3] <> 3) then 
+        passed := False;
+
+    if passed then
+        writeln('Wszystkie testy ukonczono pomyslnie.')
+    else
+        write('Blad w testach.');
+
+end;
+
+
 begin
     randomizer(liczby, 10, 50, 25);
     bubbleSort(liczby, 25);
+    unitTest();
 end.
