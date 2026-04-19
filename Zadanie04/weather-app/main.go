fbe72261654cbeba33c2833ae977eab7128fdcbf
Medo <xmedo117@proton.me>
2026-04-19 19:17:21 +0200

    zadanie04 - 4.0

--- Zadanie04/weather-app/main.go
+++ Zadanie04/weather-app/main.go
@@ -5,12 +5,42 @@ import (
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"encoding/json"
 )
 
 type Weather struct {
 	gorm.Model
 	City string  `json:"city"`
 	Temperature float64 `json:"temperature"`
+	Windspeed float64 `json:"windspeed"`
+	Isday int `json:"is_day"`
+}
+
+type ExternalWeatherData struct {
+	CurrentWeather struct {
+		Temperature float64 `json:"temperature"`
+		Windspeed float64 `json:"windspeed"`
+		IsDay int `json:"is_day"`
+	} `json:"current_weather"`
+}
+
+
+type WeatherProxy struct{}
+
+func (p *WeatherProxy) FetchWeather(city string) (float64, float64, int, error) {
+	url := "https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&current_weather=true"
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	defer resp.Body.Close()
+
+	var data ExternalWeatherData
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return 0, 0, 0, err
+	}
+
+	return data.CurrentWeather.Temperature, data.CurrentWeather.Windspeed, data.CurrentWeather.IsDay, nil
 }
 
 var DB *gorm.DB
@@ -22,18 +52,21 @@ func initDB() {
 		panic("Blad - baza danych")
 	}
 	DB.AutoMigrate(&Weather{})
-
-	var count int64
-	DB.Model(&Weather{}).Count(&count)
-	if count == 0 {
-		initialWeather := Weather{City: "Krakow", Temperature: 20.0}
-		DB.Create(&initialWeather)
-	}
 }
 
 func GetWeather(c echo.Context) error {
-	var weather Weather
-	DB.First(&weather)
+	proxy := WeatherProxy{}
+	temp, windspeed, is_day, err := proxy.FetchWeather("Krakow")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Proxy failed"})
+	}
+
+	weather := Weather{
+		City: "Krakow",
+		Temperature: temp,
+		Windspeed: windspeed,
+		Isday: is_day,
+	}
 	return c.JSON(http.StatusOK, weather)
 }
 
