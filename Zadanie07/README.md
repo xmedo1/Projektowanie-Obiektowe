d598aef77406f024ff062981f5f499485caeb952
Medo <xmedo117@proton.me>
2026-05-25 17:40:44 +0200

    zadanie07 - swift vapor init

---
+++ Zadanie07/README.md
@@ -0,0 +1,14 @@
+**Zadanie 7**
+
+Proszę napisać prostą aplikację w Vaporze, wykorzystując Leaf jako
+silnik szablonów or Fluent jako ORM. Proszę stworzyć trzy modele oraz
+CRUD dla każdego z nich. Należy stworzyć model z minimum jedną
+relacją. CRUD powinien mieć odzwierciedlenie w szablonach.
+
+:x: 3.0 Należy stworzyć kontroler wraz z modele Produktów zgodny z CRUD w ORM Fluent \
+:x: 3.5 Należy stworzyć szablony w Leaf \
+:x: 4.0 Należy stworzyć drugi model oraz kontroler Kategorii wraz z relacją \
+:x: 4.5 Należy wykorzystać Redis do przechowywania danych \
+:x: 5.0 Wrzucić aplikację na heroku \
+
+https://www.heroku.com/github-students
---
+++ Zadanie07/ShopApp/Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "ShopApp",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+        // 🗄 An ORM for SQL and NoSQL databases.
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        // 🪶 Fluent driver for SQLite.
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.6.0"),
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "ShopApp",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "ShopAppTests",
+            dependencies: [
+                .target(name: "ShopApp"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+] }
