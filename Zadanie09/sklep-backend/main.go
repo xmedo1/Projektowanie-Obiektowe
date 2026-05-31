62297cfaac718241697ec2af99f31dcf1076626b
Medo <xmedo117@proton.me>
2026-05-31 16:56:14 +0200

    cors fix

--- Zadanie09/sklep-backend/main.go
+++ Zadanie09/sklep-backend/main.go
@@ -56,6 +56,12 @@ func main() {
 
 	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
 		enableCors(&w)
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		products := []Product{
 			{ID: 1, Name: "Jablko", Price: 1.2},
 			{ID: 2, Name: "Banan", Price: 1.5},
@@ -67,6 +73,12 @@ func main() {
 
 	http.HandleFunc("/payments", func(w http.ResponseWriter, r *http.Request) {
 		enableCors(&w)
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		if r.Method == "POST" {
 			var data map[string]interface{}
 			if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
@@ -156,6 +168,6 @@ func main() {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
 }
